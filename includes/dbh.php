cba65a571a6e5aa283f64e9549408193864cc7b7
tomitomazic15 <tomitomazic15@gmail.com>
2021-10-19 01:14:32 +0200

    fix

--- includes/dbh.php
+++ includes/dbh.php
@@ -2,7 +2,7 @@
 
  $dbhost = "localhost";
  $dbuser = "tomi_user";
- $dbpass = "pas123sword";
+ $dbpass = "";
  $db = "tomi_spletna";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
